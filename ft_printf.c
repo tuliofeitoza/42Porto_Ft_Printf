cd01406c87485118eb6e815d196d9d99811d27b1
Tulio Feitoza <tsilva-f@c1r1s1.42porto.com>
2024-05-13 23:53:01 +0100

    progresso 10%

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,34 @@
 /*   By: tsilva-f <tsilva-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 21:11:22 by tsilva-f          #+#    #+#             */
-/*   Updated: 2024/05/11 03:18:07 by tsilva-f         ###   ########.fr       */
+/*   Updated: 2024/05/13 23:43:26 by tsilva-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int ft_printf(const char *, ...)
+static void check(va_list args, const char *content, int *i)
 {
     
+}
+
+int ft_printf(const char *content, ...)
+{
+    va_list args;
+    unsigned int iprint;
+
+    iprint = 0; 
+    va_start(args, content);
+    
+    while (content[iprint])
+    {
+        if (content[iprint] == '%')
+        {
+            iprint++;
+            
+        }
+        else
+            iprint = iprint + put_char(content[iprint]);
+        iprint++;
+    }     
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: tsilva-f <tsilva-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 21:11:57 by tsilva-f          #+#    #+#             */
-/*   Updated: 2024/05/09 21:18:52 by tsilva-f         ###   ########.fr       */
+/*   Updated: 2024/05/13 18:57:32 by tsilva-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+# include <stdlib.h>
 
 int ft_printf(const char *, ...);
 
