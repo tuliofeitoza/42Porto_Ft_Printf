0aa72341fb13808148d1c7a0b316536aa7db6f90
tsilva-f <tuliosfeitoza@gmail.com>
2024-05-14 19:47:05 +0100

    str

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: tsilva-f <tsilva-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 21:11:22 by tsilva-f          #+#    #+#             */
-/*   Updated: 2024/05/14 15:50:58 by tsilva-f         ###   ########.fr       */
+/*   Updated: 2024/05/14 19:20:12 by tsilva-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void    ft_put_char(char c, int *i)
+void    ft_putchar(char c, int *i)
 {
     write(1, &c, 1);
     *i += 1;
@@ -26,9 +26,9 @@ static void ft_check(const char *content, va_list args, int *i)
         {
             content++;
             if (content == 'c')
-                ft_put_char(va_arg(arg, int), id);
+                ft_put_char(va_arg(arg, content), id);
             if (content == 's')
-                ft_put_str(va_arg(arg, char *), id)
+                ft_put_str(va_arg(arg, content), id)
         }
         else
             ft_put_char(*content, id);
